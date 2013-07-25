6022373186b43a02a26e0bd04f1fc78e2afa9bcd
Fahim Chowdhury <fahim.chowdhury@ogilvy.com>
2013-07-25 17:31:00 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+(function(window) {
+ function Main() {
+ if(window.addEventListener) {
+ window.addEventListener("load", onLoad);
+ } else {
+ window.attachEvent("onload", onLoad);
+ }
+ 
+ }
+ function onLoad() {
+ }
+ Main();
+ }
+ )(window);
